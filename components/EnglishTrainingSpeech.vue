cc3b977d8b20eb901c32236abf680f652bf2be67
ducnc <duc.nc97@gmail.com>
2024-01-23 23:56:44 +0700

    text to speech

--- components/EnglishTrainingSpeech.vue
+++ components/EnglishTrainingSpeech.vue
@@ -212,8 +212,7 @@
                 <b-button size="xs" type="submit" variant="primary">Submit</b-button>
               </div>
               <div>
-                <b-form-select size="xs" v-model="selected[1]" :options="selects" value="null"
-                  @change="genVocabulary(dayFirst, 1)"></b-form-select>
+                <b-form-select size="xs" v-model="selected[1]" :options="selects" value="null"></b-form-select>
               </div>
             </b-form>
 
